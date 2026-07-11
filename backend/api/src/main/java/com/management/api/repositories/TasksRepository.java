7fa9438be9b65dd0a637ce236c28566f58b03006
Manuel Orellana <manuel.orellana.s@usach.cl>
2026-07-11 18:21:28 -0400

    Creation of model of tasks with his services, repository and controller, the filters are not done

---
+++ backend/api/src/main/java/com/management/api/repositories/TasksRepository.java
@@ -0,0 +1,31 @@
+package com.management.api.repositories;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.scheduling.config.Task;
+
+import com.management.api.models.Tasks;
+
+@Repository
+public interface TasksRepository extends JpaRepository<Tasks, Long>{
+
+    List<Tasks> findByUser_Id_auth(Long userId);
+
+    @Query(value = "SELECT t.* FROM tasks t " + "JOIN sectors s ON t.sector_id = s.id " +
+           "JOIN auth_user u ON t.user_id = u.id_auth " +
+           "WHERE t.user_id = ?1 AND t.completed = false " +
+           "ORDER BY ST_Distance(s.location, u.location) ASC LIMIT 1", 
+           nativeQuery = true)
+    Tasks findNearestPendingTask(Long userId);
+
+    @Query(value = "SELECT COALESCE(AVG(ST_Distance(s.location, u.location)), 0) " +
+           "FROM tasks t " +
+           "JOIN sectors s ON t.sector_id = s.id " +
+           "JOIN auth_user u ON t.user_id = u.id_auth " +
+           "WHERE t.user_id = ?1 AND t.completed = true", 
+           nativeQuery = true)
+    Double getAverageDistanceOfCompletedTasks(Long userId);
+}
